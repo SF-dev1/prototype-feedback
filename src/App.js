eddcd8c53bf7c9ef0585c4a2fdf1ec3eaf7ca3ea
Jay Chauhan <142774567+SF-dev1@users.noreply.github.com>
2024-02-03 11:19:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import FeedbackForm from './FeedbackForm';
+
+export default function Layout() {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <center>
+            <Card className={`${windowWidth >= 768 ? 'w-50' : ''}`}>
+                <Card.Img variant="top" src="/img/Prototype Review.webp" />
+                <Card.Body>
+                    <FeedbackForm width={windowWidth} />
+                </Card.Body>
+            </Card>
+        </center>
+    );
+}
